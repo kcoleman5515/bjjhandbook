eed6d0430c679cee46330ae538324b1339b0010a
Kyle Coleman <kcoleman20@cnm.edu>
2022-08-11 15:54:25 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+
+  public boolean sideControl() {
+    return sideControl();
+  }
+
+  public boolean openGuard() {
+    return openGuard();
+  }
+
+  public boolean closedGuard() {
+    return closedGuard();
+  }
+
+  public boolean takeDown() {
+    return takeDown();
+  }
+
+  public boolean butterflyGuard() {
+    return butterflyGuard();
+  }
+
+  public boolean deLaRivaGuard() {
+    return deLaRivaGuard();
+  }
+
+  public boolean reverseDeLaRivaGuard() {
+
+  }
+}
